d30c4514c08aba5db4a18ed9e9b8721676d26415
Student <student@lan.itstep.kiev.ua>
2017-09-14 21:08:30 +0300

    js

---
+++ 001_Introduction/first.js
@@ -0,0 +1,18 @@
+alert("main")
+
+var a = 45;
+var b = 34;
+var c = 21;
+var d = 8;
+var e = 15;
+var f = 18;
+
+var result0 = a ** b;
+var result1 = c ** d;
+var result2 = e ** f;
+
+document.write("a + b = " + result0 + "<br />")     
+document.write("c + d = " + result1 + "<br />") 
+document.write("e + f = " + result2 + "<br />")   
+
+
---
+++ 001_Introduction/somename.js
@@ -0,0 +1,6 @@
+alert("in modal window");
+document.write("<h1>in document</h1>");
+console.log('This will be out in concole');
+
+
+
